8a075a8afa29187d7b4061deb15c3d57e0ba7801
Tee O <tdopress@gmail.com>
2023-06-03 16:15:45 -0400

    initial commit

---
+++ javascript-algorithms-scotchio/Beginner/reverseString/index-START.js
@@ -0,0 +1,15 @@
+/* CHALLENGE
+Given a string of text, write an algorithm that returns the text received in a reversed format. 
+E.g reverseString('algorithms') // should return 'smhtirogla'
+*/
+
+
+
+function reverseString(text) {
+    // Built in methods
+    return text.split('').reverse().join('')
+}
+
+
+
+module.exports = reverseString
