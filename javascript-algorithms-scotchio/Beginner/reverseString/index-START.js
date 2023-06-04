90ea6fcdad9db29d42663277d1cfb821a2fc0224
Tee O <tdopress@gmail.com>
2023-06-04 12:30:57 -0400

    finished problem

--- javascript-algorithms-scotchio/Beginner/reverseString/index-START.js
+++ javascript-algorithms-scotchio/Beginner/reverseString/index-START.js
@@ -6,8 +6,33 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 
 
 function reverseString(text) {
-    // Built in methods
-    return text.split('').reverse().join('')
+    // BUILT IN METHODS
+    // return text.split('').reverse().join('')
+
+    //FOR LOOP
+    // let result = ''
+
+    //Regular
+    // for (let i = text.length - 1; i >= 0; i--) {
+    //    result += text[i]
+    // }
+
+    //Es 6
+    // for (let char of text) {
+    //     result = char + result
+    // }
+
+    // return result
+
+    //RECURSION
+    // if (text === '') {
+    //     return ''
+    // } else {
+    //     return reverseString(text.substr(1)) + text[0]
+    // }
+
+    //REDUCE
+    return text.split('').reduce((acc, char) => char + acc, '')
 }
 
 
